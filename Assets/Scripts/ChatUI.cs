bd108e15a4701edb36704243e1baf532e37339db
JiHun <pgh979@gmail.com>
2025-01-01 18:02:56 +0900

    키워드 텍스트 추가

--- Assets/Scripts/ChatUI.cs
+++ Assets/Scripts/ChatUI.cs
@@ -1,7 +1,17 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class ChatUI : MonoBehaviour
 {
+    //public TMP_Text chatText;
+    public TextMeshProUGUI chatText;
+
+    private void Start()
+    {
+        chatText.text = "제대로 안그리면 죽여버리겠다!\r\n\r\n키워드 : "+GameManager.Instance.keyWord;
+    }
+
     public void OnClickCloseButton()
     {
         Time.timeScale = 1;
